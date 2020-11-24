1e351ecd539f2892d01ba2db82790221d74faec0
intecfrontend <53944321+intecfrontend@users.noreply.github.com>
2020-11-24 11:15:15 +0100

    first commit

---
+++ src/Navbar.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+export class Navbar extends Component {
+  render() {
+    return (
+      <div>
+        <nav className="NavbarItems">
+<div className="navbar-logo">LOGO</div>
+
+        </nav>
+      </div>
+    )
+  }
+}
+
+export default Navbar
